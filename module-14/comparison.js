cebaf1956a7ae584e88c32bf793a986a710f0fd3
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-23 22:16:46 +0600

    feat: comparison

---
+++ module-14/comparison.js
@@ -0,0 +1,24 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(5 >= 10);
+
+
+// var peyaraPrice = 40;
+// var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+// console.log(peyaraPrice < applePrice);
+
+/**
+ * ==
+ * ===
+ */
+
+// console.log(10 === '10'); type check
+// console.log(10 == '10');
+
+//---------------
+console.log(10 != '10');
+console.log(10 !== '10'); //type check
---
+++ module-14/variable.js
@@ -0,0 +1,21 @@
+// Variable ==> container
+
+//number
+var sunglassPrice = 80;
+
+//string 
+var subject = 'EEE';
+
+//boolean
+var passed = true;
+
+// Naming convention
+// 1. single word
+// 2. no quote
+// 3. no gap or dash
+// 4. can not start number
+// 5. can not be a keyword
+// 6. prefer camelCase
+
+var lastExamResult = 89;
+
