43ab21e51815136917b48e1a9cc90cf8972601ae
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-24 15:49:36 +0600

    feat: ternary

---
+++ module-14/ternary_2.js
@@ -0,0 +1,19 @@
+const isLeader = true;
+//if(isLeader === true)
+if(isLeader){
+    console.log('leader asche! chair charo');
+}
+else{
+    console.log('vitore dukte dibi na');
+}
+//
+
+
+const isPassed = true;
+//if(isPassed === false)
+if(!isPassed){
+    console.log('toke biye diya dibo');
+}
+else{
+    console.log('tumi amder noyon er moni')
+}
