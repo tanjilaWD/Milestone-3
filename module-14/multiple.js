593ea44c31ccd65ce00ff43bedfc464ffd6d53d2
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-24 09:41:05 +0600

    feat: multiple condition

---
+++ module-14/multiple.js
@@ -0,0 +1,22 @@
+const salary = 25000;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+// if(salary > 20000 && height > 66){
+//     console.log('Su-----patro');
+// }
+// else{
+//     console.log('onno patro khuji')
+// }
+
+// if(salary > 24000 || height > 72){
+//     console.log('eso baba kobul')
+// }
+// else{
+//     console.log('vaag tui mokbul')
+// }
+
+// ---------Complex Condition-----------
+if((salary > 25000 && hasCar == true) || isBCS == true){
+    console.log('raji')
+}
