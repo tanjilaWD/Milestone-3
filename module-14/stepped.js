bf52f8aa5fa087d5a2cdef079ec9048d7d4271b9
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-24 10:01:08 +0600

    feat: multiple level condition

---
+++ module-14/stepped.js
@@ -0,0 +1,19 @@
+/**
+ * Multiple level condition
+ */
+const price = 3000;
+if(price >= 5000){
+    //10% discount
+    const discount = price * 10 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else if(price > 2500){
+    //5% discount
+    const discount = price * 5 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else{
+    console.log(price);
+}
