226e8b23eab6162b208acd58984734d68bb29872
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-24 22:28:54 +0600

    feat: include, indexOf, isArry

---
+++ module-14.5/include.js
@@ -0,0 +1,10 @@
+const friends = ['a', 'b', 'c', 'd'];
+console.log(friends.includes('c'));
+console.log(friends.includes('o'));
+
+if(friends.includes('b')){
+    console.log('bithi')
+}
+else{
+    console.log('no food. we are fasting')
+}
---
+++ module-14.5/indexOf.js
@@ -0,0 +1,3 @@
+const friends = ['a', 'b', 'c', 'd'];
+console.log(friends.indexOf('c'))
+console.log(friends.indexOf('f')) // -1
