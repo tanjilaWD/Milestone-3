afc7cd26c75ca3649197bc9800edc55b10e0376c
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-27 22:35:40 +0600

    feat: string

---
+++ module-16/string.js
@@ -0,0 +1,21 @@
+const country = 'Bangladesh';
+const division = "Newkale";
+const district = `B-Baria`;
+// const thana = new String('Demra');
+// console.log(thana);
+
+// const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers);
+// numbers[1] = 11;
+// console.log(numbers);
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+//string is immutable --> not changeable
+// array is mutable --> you can change the array elements
+
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[2]);
+console.log(capital);
+
