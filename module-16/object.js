b6a5d898f89d5a4f14bf8059da63915a67a3a157
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-28 19:55:45 +0600

    feat: object

---
+++ module-16/object.js
@@ -0,0 +1,22 @@
+// primitive
+const age = 21;
+
+// object
+// non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'aaaa',
+    examDate: '30 Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
