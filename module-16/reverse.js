ef6817de8edf5b2ddeb1238288d5f966aeec8db5
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-28 10:39:15 +0600

    feat: reverse

---
+++ module-16/reverse.js
@@ -0,0 +1,12 @@
+const sentence = 'I am learning Web Dev.'
+
+// for(const letter of sentence){
+//     console.log(letter);
+// }
+
+// let reverse = ''
+// for(const letter of sentence){
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
