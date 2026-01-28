e7d480d0f5d99632eca8fc043d4bddd92c33550b
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-28 22:21:16 +0600

    feat: loop

---
+++ module-16/loop.js
@@ -0,0 +1,20 @@
+const mobile = {
+    brand : 'sumsung',
+    price: 25000,
+    color: 'black',
+    camera: '12mp',
+    isNew: true
+}
+// for of : array
+// for in : object
+for(const prop in mobile){
+    // console.log(prop);
+    // console.log(mobile[prop]);
+}
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':', mobile[key]);
+}
