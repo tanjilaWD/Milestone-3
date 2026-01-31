0b094a558b260c675f18df6c413e8879e6f9134f
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-31 18:37:43 +0600

    function declare

---
+++ module-17/function.js
@@ -0,0 +1,12 @@
+/**
+ * function name(){
+ *    }
+ */
+// function declare
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log(' paste');
+    console.log('2 min goso and goso and fena felo');
+}
+// call the function
+brushTeeth();
