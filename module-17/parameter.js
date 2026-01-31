2cdd203896ed7bb479cadbcf990366ec66cd02e2
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-31 19:26:27 +0600

    function

---
+++ module-17/parameter.js
@@ -0,0 +1,21 @@
+function square (number){
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is:', borgo);
+}
+square(4);
+
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(5, 71);
+
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+addAll(3, 4, 6, 7, 2);
