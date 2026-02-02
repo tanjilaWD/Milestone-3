d4c0c686f7d182bebdb1bb14a7630d0e4872aa97
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-02 18:40:46 +0600

    math

---
+++ module-18/math.js
@@ -0,0 +1,22 @@
+const min = Math.min(45, 21, 65, 99, 126);
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-10)); 
+
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+
+console.log(Math.floor(9.57));
+
+console.log(Math.ceil(2.99995));
+console.log(Math.ceil(5.00001));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand);
