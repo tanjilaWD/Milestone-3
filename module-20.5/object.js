84a264dd7a9c770c23105da89b4533d0f13b37e8
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-08 21:02:03 +0600

    object

---
+++ module-20.5/object.js
@@ -0,0 +1,18 @@
+const nayok ={
+    name: 'Sakib khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingel: true,
+    friends: ['Apu', 'Raaz', 'Salman', 'Amir'],
+    movies: [{name: 'no.1', year: 2015}, {name: 'King khan', year: 2018}],
+    act: function(){
+        console.log('acting like sakib khan')
+    },
+    car: {
+        brand: 'tesla',
+        price: 50000000,
+        made: 2025
+    }
+}
+// console.log(student);
+nayok.act();
